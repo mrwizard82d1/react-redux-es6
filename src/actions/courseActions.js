c6296498c3157cb6a03bbe37f3f83d3163927a19
Larry Jones <larryj@blinds.com>
2017-04-23 15:32:50 -0500

    Create first Redux action creator

---
+++ src/actions/courseActions.js
@@ -0,0 +1,18 @@
+/**
+ * Created by larryjones on 4/23/17.
+ */
+
+// The (Redux) actions related to courses.
+
+export function createCourse(course) {
+  // The returned object takes advantage of an ES6 feature: if the initial value of an object comes from a variable
+  // whose name is the same as the property which it initializes, one can leave it out. (Think the opposite of
+  // Clojure / ClojureScript map destructuring in a `let`.)
+  //
+  // Concretely, I want my action to have two properties: `type` (required for a Redux action creator) and `course`
+  // containing the course to be created. Because the variable, `course`, has the same name as the property, I simply
+  // include the variable in the object literal and the engine takes care of the mapping.
+  //
+  return {type: 'CREATE_COURSE', course};
+}
+
