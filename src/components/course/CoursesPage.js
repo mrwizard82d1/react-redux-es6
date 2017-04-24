bc75113917fcbf167b0a8cd6d7095524b1516580
Larry Jones <larryj@blinds.com>
2017-04-24 14:38:28 -0500

    Removed addition feature...
    ... in preparation for putting it in its own React component.

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -17,41 +17,6 @@ class CoursesPage extends React.Component {
   // Initialize this React component with properties and a(n React) context.
   constructor(props, context) {
     super(props, context);
-
-    // The state of an instance consists, for now, of a single course with a single property identified by "title."
-    this.state = {
-      course: {title: ""}
-    };
-
-    // Previously, the value assigned to `this` in the `changeTitle` method depended on the "JavaScript this rules"
-    // (https://github.com/getify/You-Dont-Know-JS/blob/master/this%20%26%20object%20prototypes/ch2.md). Specifically,
-    // since a UI element invoked `changeTitle`, `this`, within that method, referred to `undefined`.
-    // Since the UI element did not, hopefully, have a state method, the browser warned us:
-    //
-    // Uncaught TypeError: Cannot read property 'state' of undefined.
-    //
-    // To repair this error, I must explicitly bind this when invoking this method.
-    this.changeTitle = this.changeTitle.bind(this);
-
-    // To correctly use `saveChanges`, I must bind the `this` argument similarly.
-    this.saveChanges = this.saveChanges.bind(this);
-  }
-
-  // Change the title in response to the supplied event.
-  changeTitle(event) {
-    // Get the course so we can use it to update the state later.
-    const course = this.state.course;
-
-    // Change the course title to the new title.
-    course.title = event.target.value;
-
-    // Set (update) the state of this (React) component.
-    this.setState({course: course});
-  }
-
-  // Save any changes the user made to the course.
-  saveChanges(event) {
-    this.props.actions.createCourse(this.state.course);
   }
 
   // Returns markup for a single course row.
@@ -67,13 +32,6 @@ class CoursesPage extends React.Component {
       <div>
         <h1>Courses</h1>
         {this.props.courses.map(this.courseRow)}
-        <h2>Add Course</h2>
-        <input type="text"
-               onChange={this.changeTitle}
-               value={this.state.course.title} />
-        <input type="submit"
-               value="Save"
-               onClick={this.saveChanges} />
       </div>
     );
   }
