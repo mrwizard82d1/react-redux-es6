edeba007a7cc6804e3a78dac9986f23a1b48c0a1
Larry Jones <larryj@blinds.com>
2017-04-23 19:59:00 -0500

    Configure the Redux application store

---
+++ src/store/configureStore.js
@@ -0,0 +1,31 @@
+/**
+ * Created by larryjones on 4/23/17.
+ */
+
+// Defines a function to create our configured application store. Our application entry point will call this function
+// at "system start" create and configure the store at "system start."
+
+import {createStore, applyMiddleware} from "redux";
+
+// To configure our application store, we must supply the root reducer we created previously.
+import rootReducer from "../reducers/index";
+
+// This middleware wil warn us if we (inadvertently) change the application state in our JavaScript code.
+import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
+
+// Create the newly configured application store.
+//
+// One can use `initialState` to specify the initial state of the store. Specifying the initial state is, apparently,
+// very useful in scenarios employing server side rendering.
+export default function configureStore(initialState) {
+  // To configure this store, we must supply the root reducer, this allows the store to delegate actions to the root
+  // reducer which in turn, delegates to our indivdual reducers.
+  //
+  // Finally, we supply middleware. The specific middleware we use ensures that we cannot inadvertently change the
+  // state without going through 'setState'.
+  return createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(reduxImmutableStateInvariant())
+  );
+}
