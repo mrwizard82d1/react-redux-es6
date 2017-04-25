68714f3e63241597da66cf9e882344c9e205bf90
Larry Jones <larryj@blinds.com>
2017-04-25 13:45:24 -0500

    Integrate new action into app

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,12 +5,14 @@
 import {combineReducers} from "redux";
 // I choose to name the export `courses` even though the original function was named `courseReducer`. By naming the
 // export `courses` and then using that name in `combineReducers`, we will use the name `courses` when we reference
-// the courses part of the state.
+// the courses part of the state. I made a similar choice about `authors`.
 import courses from "./courseReducer";
+import authors from "./authorReducer";
 
 const rootReducer = combineReducers({
   // Again, we use the ES6 "shorthand property name" that "automagically" names a property after the name of the
   // value used to create the property.
+  authors,
   courses
 });
 
