faa5973095f606c9bdbd048ccfd67c450cb30711
Larry Jones <larryj@blinds.com>
2017-04-27 01:43:57 -0500

    Create AJAX status actions

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -2,10 +2,14 @@
  * Created by larryjones on 4/24/17.
  */
 
-// Defines the constant action types.
-//
-// Putting these constants in this file is a compromise between a "global" constants file (that couples all consumers
-// to the single file) and exporting constants from the actions file itself (`courseActions.js` in our scenario).
+/**
+ * Defines the action types in our system.
+ *
+ * Putting these constants in this file is a compromise between a "global" constants file (that couples all consumers
+ * to the single file) and exporting constants from the actions file itself (`courseActions.js` in our scenario).
+ */
+
+export const BEGIN_AJAX_CALL = "BEGIN_AJAX_CALL";
 
 export const LOAD_COURSES_SUCCESS = "LOAD_COURSES_SUCCESS";
 export const LOAD_AUTHORS_SUCCESS = "LOAD_AUTHORS_SUCCESS";
---
+++ src/actions/ajaxStatusActions.js
@@ -0,0 +1,14 @@
+/**
+ * Created by larryjones on 4/27/17.
+ */
+
+import * as types from "./actionTypes";
+import initialState from "../reducers/initialState";
+
+/**
+ * Action to fire when an AJAX call begins.
+ */
+export function beginAjaxCall (state=initialState) {
+  return {type: types.BEGIN_AJAX_CALL};
+}
+
