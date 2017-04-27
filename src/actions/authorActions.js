5f2e0f778906884ee3a2f46a2d95ba968adccb27
Larry Jones <larryj@blinds.com>
2017-04-27 01:46:21 -0500

    Call `beginAjaxCall` in thunks

--- src/actions/authorActions.js
+++ src/actions/authorActions.js
@@ -6,21 +6,30 @@
 
 import * as types from "./actionTypes";
 import AuthorApi from "../api/mockAuthorApi";
+import {beginAjaxCall} from "./ajaxStatusActions";
 
 
-// The action invoked if all authors successfully loaded from an external source.
-//
-// This action is only invoked if all authors were asynchronously loaded from the external source.
+/**
+ * Return the load authors action.
+ * @param authors - The successfully loaded authors.
+ * @returns {{type, authors: *}} - The load author action.
+ */
 export function loadAuthorsSuccess(authors) {
   return {type: types.LOAD_AUTHORS_SUCCESS, authors};
 }
-// The function to asynchronously load authors from a remote source.
-//
-// Remember that this function is a higher order function: it returns a function accepting a `dispatch`. This outer
-// function, a thunk, invokes `AuthorApi.getAllAuthors` handling the `Promise` by invoking `dispatch` the `Promise` is
-// resolved (that is, when all authors are downloaded and available).
+
+/**
+ * Asynchronously loads authors from a remote source.
+ * @returns {function(*)} The thunk that, when invoked, will download authors and dispatch the action when the authors
+ * are successfully downloaded.
+ *
+ * Remember that this function is a higher order function: it returns a function accepting a `dispatch`. This outer
+ * function, a thunk, invokes `AuthorApi.getAllAuthors` handling the `Promise` by invoking `dispatch` the `Promise` is
+ * resolved (that is, when all authors are downloaded and available).
+ */
 export function loadAuthors() {
   return dispatch => {
+    dispatch(beginAjaxCall);
     return AuthorApi.getAllAuthors()
       .then(authors => {
         dispatch(loadAuthorsSuccess(authors));
--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -10,31 +10,56 @@ import * as types from "./actionTypes";
 
 // Import the (mock) course API.
 import courseApi from "../api/mockCourseApi";
+import {beginAjaxCall} from "./ajaxStatusActions";
 
-// Create an action assuming successfully loaded from external API.
-//
-// This particular action only fires if the API successfully loaded all the courses. We have, at least for now, chosen
-// very simple error handling (throwing an error). However, in production, we might create another action called, for
-// example, `loadCoursesError` or `loadCoursesFailure`. By including the "Successful" (or perhaps better, "Succeeded,")
-// suffix, we clarify that this action *only* occurs if loading the courses (asynchronously) was successful.
+/**
+ * Create an action assuming successfully loaded from external API.
+ *
+ * @param courses - The successfully loaded courses.
+ * @returns {{type, courses: *}} - A Redux action containing a type and the successfully downloaded courses.
+ *
+ * This particular action only fires if the API successfully loaded all the courses. We have, at least for now, chosen
+ * very simple error handling (throwing an error). However, in production, we might create another action called, for
+ * example, `loadCoursesError` or `loadCoursesFailure`. By including the "Successful" (or perhaps better, "Succeeded,")
+ * suffix, we clarify that this action *only* occurs if loading the courses (asynchronously) was successful.
+ */
 function loadCoursesSuccessful(courses) {
   return {type: types.LOAD_COURSES_SUCCESS, courses};
 }
 
+/**
+ * Create an action indicating that a course was successfully updated.
+ *
+ * @param course - The updated course.
+ * @returns {{type, course: *}} - The action to be dispatched.
+ *
+ */
 function updateCourseSuccess(course) {
   return {type: types.UPDATE_COURSE_SUCCESS, course};
 }
 
+/**
+ * Create an action indicating that a course was successfully created.
+ *
+ * @param course - The created course.
+ * @returns {{type, course: *}} - The action to be dispatched.
+ *
+ */
 function createCourseSuccess(course) {
   return {type: types.CREATE_COURSE_SUCCESS, course};
 }
 
-// Load courses from an "external" API.
-//
-// This function is a higher order function that *returns* a function accepting a `dispatch`.
+/**
+ * Loads courses from an "external" API.
+ *
+ * @returns {Function} - Returns a dispatch function. Because this function involves an asynchronous call, the returned
+ * function is actually a "thunk" that, when invoked, will invoke the asynchronous call and will then dispatch the
+ * appropriate action when the call's results are available.
+ */
 export function loadCourses() {
   // Returning a function accepting a `dispatch` function is boilerplate present in every action involving a thunk.
   return function(dispatch) {
+    dispatch(beginAjaxCall());
     return courseApi.getAllCourses()
       .then(courses => {
         dispatch(loadCoursesSuccessful(courses));
@@ -45,9 +70,11 @@ export function loadCourses() {
   };
 }
 
-// Saves a course.
-//
-// This function is a higher order function that *returns* a function accepting a `dispatch`.
+/**
+ * Saves a course
+ * @param course - The (changed) course to be saved.
+ * @returns {Function} - The "thunk" wrapping an asynchronous function that dispatches an action when complete.
+ */
 export function saveCourse(course) {
   // Although not used in this example, `getState` is a(n optional) function that allows this thunk to get the current
   // state of the store.
@@ -55,6 +82,7 @@ export function saveCourse(course) {
   // Because this function has the 'course` parameter set to the course to be changed, we do not need to use `getState`;
   // however, in some usage scenarios, the caller may not have the updated (partial) state.
   return function(dispatch, getState) {
+    dispatch(beginAjaxCall());
     return courseApi.saveCourse(course)
       .then(savedCourse => {
         // If the course already exists (that is, `course.id` is truthy), dispatch the update course successful action.
