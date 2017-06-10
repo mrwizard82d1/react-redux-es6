9724cb6196ca44ab6741cd898612d9321b644091
Larry Jones <larryj@blinds.com>
2017-06-10 13:26:28 -0500

    A starting point to test a connected component

--- src/components/course/ManagecCoursePage.test.js
+++ src/components/course/ManagecCoursePage.test.js
@@ -3,14 +3,22 @@
  */
 
 import React from 'react';
+import { Provider } from 'react-redux';
 import { expect } from 'expect';
 import { mount } from 'enzyme';
 import ManageCoursePage from './ManageCoursePage';
 
 describe('ManageCoursePage', () => {
   it('sets an error when saving a course with no title', () => {
-    const cut = mount(<ManageCoursePage authors="" course="" actions=""/>);
-
-    expect(cut.find('CourseForm').length).toBe(1);
+    /**
+     * The following code tests `ManagedCoursePage` as a connected component.
+     *
+     * This approach is most useful if you want to test connect-related code
+     * like `mapStateTopProps`.
+     */
+    const cut = mount(
+      <Provider store={store}>
+        <ManageCoursePage authors="" course="" actions=""/>
+      </Provider>);
   })
 });
