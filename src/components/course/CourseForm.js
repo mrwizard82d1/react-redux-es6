487305ce07af59eb6836f608807ae27b6142d9c9
Larry Jones <larryj@blinds.com>
2017-06-10 20:26:01 -0500

    Repair integration test error: no title error
    Previous commits of this file passed objects to its child components
    with a property of `errors`. Unfortunately, the child components
    expected a property named `error` (singular).
    
    This commit repairs that error.

--- src/components/course/CourseForm.js
+++ src/components/course/CourseForm.js
@@ -18,7 +18,7 @@ const CourseForm = ({course, allAuthors, onSave, onChange, saving, errors}) => {
         label="Title"
         value={course.title}
         onChange={onChange}
-        errors={errors.title} />
+        error={errors.title} />
 
       <SelectInput
         name="authorId"
@@ -27,21 +27,21 @@ const CourseForm = ({course, allAuthors, onSave, onChange, saving, errors}) => {
         defaultOption="Select Author"
         options={allAuthors}
         onChange={onChange}
-        errors={errors.authorId} />
+        error={errors.authorId} />
 
       <TextInput
         name="category"
         label="Category"
         value={course.category}
         onChange={onChange}
-        errors={errors.category} />
+        error={errors.category} />
 
       <TextInput
         name="length"
         label="Length"
         value={course.length}
         onChange={onChange}
-        errors={errors.length} />
+        error={errors.length} />
 
       <input
         type="submit"
