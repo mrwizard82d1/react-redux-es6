42c9a400a731e3df128517d0079e60e1dc4d01f7
Larry Jones <larryj@blinds.com>
2017-06-11 16:38:50 -0500

    Change `delay` in API to 0ms
    This commit reduces the delay in fulfilling our promise from our mock
    API to 0ms. This allows our unit test to pass without the "long test"
    warning.

--- src/api/delay.js
+++ src/api/delay.js
@@ -3,4 +3,4 @@
 // To have instantaneous feedback, set this value to 0. A value of 1000 better allows the page designer to duplicate
 // the customer experience of a real API or over a slow connection. Use other values to simulate longer or shorter
 // asynchronous delays.
-export default 1000;
+export default 0;
